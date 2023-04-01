7e3a24e225b1b88dcbb6d31c0dc33724be3f2aab
Emiliano Menéndez <emystein@gmail.com>
2023-04-01 09:18:19 -0300

    Convert HumanizedTime component to functional style.

--- src/main/ui/src/components/humanized_time.jsx
+++ src/main/ui/src/components/humanized_time.jsx
@@ -1,21 +1,17 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import {connect} from 'react-redux';
 import moment from 'moment';
 
-class HumanizedTime extends Component {
-
-  render() {
-    if(!this.props.time.now){
-      return (<div/>);
+function HumanizedTime({time, date}) {
+    if (!time.now) {
+        return (<div/>);
     }
-    const prefix = this.props.prefix || "";
-    const suffix = this.props.suffix || "ago";
-    const timeAgo = moment.duration(this.props.time.now.getTime() - this.props.date);
-    return (
-      <span>{prefix} {timeAgo.humanize()} {suffix}</span>
-    );
-  }
 
+    const timeAgo = moment.duration(time.now.getTime() - date);
+    
+    return (
+        <span>{timeAgo.humanize()} ago</span>
+    );
 }
 
 export default connect(({time}) => ({time}))(HumanizedTime);
