2fb19278d62095c4751655d146766d01a036e7c9
Emiliano Menéndez <emystein@gmail.com>
2023-04-03 15:25:04 -0300

    Convert MessageInput component to functional style.

--- src/main/ui/src/components/message_input.jsx
+++ src/main/ui/src/components/message_input.jsx
@@ -1,43 +1,32 @@
-import React from 'react';
-import { sendMessage } from '../actions/chat';
-import { connect } from 'react-redux';
+import React, {useState} from 'react';
+import {sendMessage} from '../actions/chat';
+import {connect} from 'react-redux';
 
-class MessageInput extends React.Component {
+function MessageInput({user, sendMessage}) {
+  const [message, setMessage] = useState(null);
 
-  constructor(props) {
-    super(props);
-    this.state = {message: ''};
-  }
-
-  onInputChange(message) {
-    this.setState({message});
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-    this.props.sendMessage(this.props.user, this.state.message);
-    this.setState({message:''});
+  const onSubmit = (event) => {
+    event.preventDefault();
+    sendMessage(user, message);
+    this.setState({message: ''});
     return false;
   }
 
-  render() {
-    return (
-
-      <div className="container-fluid">
-        <form className="form-horizontal" role="form" onSubmit={this.onSubmit.bind(this)}>
-          <div className="form-group">
-            <div className="input-group">
-              <div className="input-group-addon">Type Message</div>
-              <input type="text"
-                value={this.state.message}
-                onChange={event => this.onInputChange(event.target.value)}
-                className="form-control input-lg"></input>
-            </div>
+  return (
+    <div className="container-fluid">
+      <form className="form-horizontal" role="form" onSubmit={event => onSubmit(event)}>
+        <div className="form-group">
+          <div className="input-group">
+            <div className="input-group-addon">Type Message</div>
+            <input type="text"
+                   value={message}
+                   onChange={event => setMessage(event.target.value)}
+                   className="form-control input-lg"></input>
           </div>
-        </form>
-      </div>
-    );
-  }
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default connect(({user}) => ({user}), { sendMessage })(MessageInput);
+export default connect(({user}) => ({user}), {sendMessage})(MessageInput);
